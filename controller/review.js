ff92eef56c636168996db7b7ac3fe442664424ba
GandhiRushabh11 <gandhirushabh11@gmail.com>
2023-11-28 01:15:39 +0530

    Added getReviews,getReview,updateReview,deleteReview and routes for reviews

--- controller/review.js
+++ controller/review.js
@@ -2,6 +2,52 @@ const Product = require("../models/product.js");
 const Review = require("../models/review.js");
 const ErrorResponse = require("../utils/errorResponse.js");
 
+exports.getReviews = async (req, res, next) => {
+  if (req.params.productId) {
+    const reviews = await Review.find({
+      product: req.params.productId,
+    }).populate({
+      path: "product",
+      select: "name description",
+    });
+    if (!reviews)
+      return next(
+        new ErrorResponse(
+          `No review found with the id of ${req.params.id}`,
+          404
+        )
+      );
+    return res.status(200).json({
+      success: true,
+      count: reviews.length,
+      data: reviews,
+    });
+  } else {
+    const reviews = await Review.find();
+    res.status(200).json({
+      success: true,
+      count: reviews.length,
+      data: reviews,
+    });
+  }
+};
+exports.getReview = async (req, res, next) => {
+  const review = await Review.findById(req.params.id).populate({
+    path: "product",
+    select: "name description",
+  });
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+};
 exports.addReview = async (req, res, next) => {
   let { text, rating } = req.body;
   const product = await Product.findById(req.params.productId);
@@ -25,5 +71,42 @@ exports.addReview = async (req, res, next) => {
   res.status(201).json({
     success: true,
     data: review,
+  });
+};
+exports.updateReview = async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  review.save();
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+};
+exports.deleteReview = async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  await review.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    data: {},
   });
 };
--- routes/review.js
+++ routes/review.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const { addReview } = require("../controller/review.js");
+const {
+  addReview,
+  deleteReview,
+  updateReview,
+  getReview,
+  getReviews,
+} = require("../controller/review.js");
 const router = express.Router({ mergeParams: true });
 const { protect } = require("../middleware/auth.js");
-
-router.post("/", protect, addReview);
-
+router.route("/").post(protect, addReview).get(getReviews);
+router
+  .route("/:id")
+  .put(protect, updateReview)
+  .delete(protect, deleteReview)
+  .get(getReview);
 module.exports = router;
